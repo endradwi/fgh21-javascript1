9a1d56e80f9dfb641cf47feb2383442d0ebabbf7
endradwi <94545255+endradwi@users.noreply.github.com>
2024-06-03 15:22:12 +0700

    Add files via upload

--- text3.js
+++ text3.js
@@ -1,7 +1,13 @@
-for ( i = 5; i > 0; i--) {
+const printSegitiga = 0
+
+if (typeof printSegitiga === "number"){
+for ( let i = 5; i > printSegitiga ; i--) {
     let row = "";
     for ( j = 1; j <= i; j++) {
          row += j + " ";
     }
     console.log(row)
+}
+}else {
+    console.log("Data harus number")
 }
